e7e9303cdca11075cc3c9b7c1450a76a8577caa4
Bryn La Follette <arcanafex@gmail.com>
2026-03-07 17:43:20 +0100

    Adds XMLBuilder and EXIFr packages, fixes data assignment

--- src/metadata.ts
+++ src/metadata.ts
@@ -10,14 +10,14 @@ export async function readImageMetadata(buffer: ArrayBuffer, filename?: string):
     
     const data = await exifr.parse(buffer, {xmp: true, iptc: true, exif: true})
 
-    const title = data?.title.value             //XMP
-        || data?.ObjectName.value               //IPTC
-        || data?.XPTitle.value                  //Windows EXIF
+    const title = data?.title?.value             //XMP
+        || data?.ObjectName?.value               //IPTC
+        || data?.XPTitle?.value                  //Windows EXIF
         || undefined
 
-    const description = data?.description.value //XMP
-        || data?.CaptionAbstract.value          //IPTC
-        || data?.ImageDescription.value         //EXIF
+    const description = data?.description?.value //XMP
+        || data?.CaptionAbstract?.value          //IPTC
+        || data?.ImageDescription?.value         //EXIF
         || undefined
         
     const tags = data?.subject                  //XMP
